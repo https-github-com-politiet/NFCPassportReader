df2c9946fc69d07ae56d48e305286610f827339d
Jenny Tellne <jenny.tellne@cygni.se>
2021-12-13 11:18:47 +0100

    ref: remove unused document types

--- Sources/NFCPassportReader/Models/DocumentType.swift
+++ Sources/NFCPassportReader/Models/DocumentType.swift
@@ -10,32 +10,22 @@ import Foundation
 public enum DocumentType {
     case ordinaryPassport
     case nationalIdCard
-    case visa
     case unknown
 
     // Special case
     case norwegianEmergencyPassport
 
     public static func toDocumentType(code: String) -> DocumentType {
-        let firstLetter = code.first
-
-        switch firstLetter {
-        case "A", "C", "I", "X":
-            return .nationalIdCard
-        case "V":
-            return .visa
-        case "P":
+        if code.first == "P" {
             return .ordinaryPassport
-        default:
-            return .unknown
         }
+        return .unknown
     }
 
     public var description: String {
         switch self {
         case .ordinaryPassport: return "Passport"
         case .nationalIdCard: return "National ID Card"
-        case .visa: return "Visa"
         case .unknown: return "Unknown"
         case .norwegianEmergencyPassport: return "Norwegian Emergency Passport"
         }
